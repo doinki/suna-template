c7700cc505cb5d7bbaa88c3c2f52dd7d0cb82672
doinki <doinki94@gmail.com>
2023-03-29 05:04:10 +0900

    🚀

--- packages/eslint-config-suna/configs/custom.js
+++ packages/eslint-config-suna/configs/custom.js
@@ -4,6 +4,7 @@ require('@rushstack/eslint-patch/modern-module-resolution');
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
+  ignorePatterns: ['dist', 'public'],
   rules: {
     'import/no-extraneous-dependencies': [
       'error',
