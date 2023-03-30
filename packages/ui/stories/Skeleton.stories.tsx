67736de1e6f84a245bb7a7f03552535b69a21fd5
doinki <doinki94@gmail.com>
2023-03-30 20:37:39 +0900

    🚀

--- packages/ui/stories/Skeleton.stories.tsx
+++ packages/ui/stories/Skeleton.stories.tsx
@@ -12,50 +12,34 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Variant: Story = {
-  render: () => (
-    <ul className="space-y-4">
-      <li>
-        <code>&quot;circular&quot;</code>
-        <Skeleton height={64} variant="circular" width={64} />
-      </li>
-      <li>
-        <code>&quot;rectangular&quot;</code>
-        <Skeleton height={64} variant="rectangular" width={160} />
-      </li>
-      <li>
-        <code>&quot;rounded&quot;</code>
-        <Skeleton height={64} variant="rounded" width={320} />
-      </li>
-      <li>
-        <code>&quot;text&quot;</code>
-        <Skeleton variant="text" />
-      </li>
-    </ul>
+  render: (args) => (
+    <div className="space-y-6">
+      <Skeleton height={64} variant="circular" width={64} {...args} />
+      <Skeleton height={64} variant="rectangular" width={160} {...args} />
+      <Skeleton height={64} variant="rounded" width={320} {...args} />
+      <Skeleton variant="text" />
+    </div>
   ),
 };
 
 export const Animation: Story = {
-  render: () => (
-    <ul className="space-y-4">
-      <li>
-        <code>&quot;pulse&quot;</code>
-        <Skeleton
-          animation="pulse"
-          height={120}
-          variant="rectangular"
-          width={120}
-        />
-      </li>
-      <li>
-        <code>false</code>
-        <Skeleton
-          animation={false}
-          height={120}
-          variant="rectangular"
-          width={120}
-        />
-      </li>
-    </ul>
+  render: (args) => (
+    <div className="space-y-6">
+      <Skeleton
+        animation="pulse"
+        height={120}
+        variant="rectangular"
+        width={120}
+        {...args}
+      />
+      <Skeleton
+        animation={false}
+        height={120}
+        variant="rectangular"
+        width={120}
+        {...args}
+      />
+    </div>
   ),
 };
 
