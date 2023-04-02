38e1adf7ad12231aea1615c61bc5e174b85765bf
doinki <doinki94@gmail.com>
2023-04-03 00:59:21 +0900

    🚀

--- apps/challenges/src/pages/_document.tsx
+++ apps/challenges/src/pages/_document.tsx
@@ -9,7 +9,7 @@ const Document = () => {
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
             __html:
-              'try{let e=localStorage.getItem("theme");"dark"===e||null===e&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.body.classList.add("dark"):document.body.classList.remove("dark")}catch(e){}',
+              'try{let e=localStorage.getItem("theme");"dark"===e||null===e&&matchMedia("(prefers-color-scheme: dark)").matches?document.body.classList.add("dark"):document.body.classList.remove("dark")}catch(e){}',
           }}
         />
       </Head>
